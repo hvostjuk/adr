5e84692b23b99f8437db05b231d4e09c26907269
Alexander Khvostyuk <hvostjuk@gmail.com>
2023-02-24 19:37:21 +0300

    fix google analytics

---
+++ pages/exam.js
@@ -0,0 +1,81 @@
+const nextQuestionBtn = document.querySelector('.next_question');
+const checkAnswerBtn = document.querySelector('.answer__btn');
+const img = document.querySelector('.content__img');
+const question = document.querySelector('.content__question');
+const answerOptions = document.querySelectorAll('.answer__option');
+const inputs = document.querySelectorAll('.answer__input');
+
+const randomArray = getRandomArray(386, 25);
+const renderArray = getRenderArray();
+
+let currentQuestion = 0;
+const answerArray = [];
+
+function getRandomArray(range, count) {
+    let m = {};
+    let randomArray = [];
+    for (let i = 0; i < count; ++i) {
+        let r = Math.floor(Math.random() * (range - i));
+        randomArray.push(((r in m) ? m[r] : r) + 1);
+        let l = range - i - 1;
+        m[r] = (l in m) ? m[l] : l;
+    }
+    return randomArray
+}
+
+function getRenderArray() {
+    let renderArray = [];
+    for (let i = 0; i < randomArray.length; i++) {
+        db.forEach(el => {
+            if (el.id === randomArray[i]) {
+                renderArray.push(el)
+            }
+        })
+    }
+    return renderArray;
+}
+
+function setStartHTML() {
+    if (renderArray[currentQuestion] !== false) {
+        img.src = `${renderArray[currentQuestion].imgUrl}`
+    }
+    question.textContent = `${renderArray[currentQuestion].question}`;
+
+    answerOptions.forEach((el, index) => {
+        el.textContent = `${renderArray[currentQuestion].answers[index].text}`;
+    });
+}
+
+function savedAnswersArray() {
+    inputs.forEach(el => {
+        if (el.checked) {
+            answerArray.push(renderArray[currentQuestion]);
+            answerArray[currentQuestion].userAnswer = `${el.id}`;
+
+        }
+    })
+}
+function clearCheckedInput() {
+    inputs.forEach(el => el.checked = false)
+}
+function setNextQuestion() {
+    if(currentQuestion === 24) {
+
+    }
+    savedAnswersArray();
+    currentQuestion = currentQuestion + 1;
+    if(renderArray[currentQuestion] != false) {
+        img.src = `${renderArray[currentQuestion].imgUrl}`;
+
+    } else {
+        img.src = ` `;
+    }
+    question.textContent = `${renderArray[currentQuestion].question}`;
+    answerOptions.forEach((el,index)=> {
+        el.textContent = `${renderArray[currentQuestion].answers[index].text}`;
+    });
+    clearCheckedInput();
+}
+setStartHTML();
+
+nextQuestionBtn.addEventListener('click', setNextQuestion);
